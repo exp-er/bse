06feb494b17ccf360507d5efb431e516368525f8
Akshat Pande <akshat.pande@outlook.com>
2023-05-17 10:06:09 +0100

    Hi

--- Main.py
+++ Main.py
@@ -1,4 +1,7 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 import BSE
 
-st.title("Bristol Stock Exchange Simulator")
+st.title('BSE Simulator')
+
+st.sidebar.radio('Select the type of simulation', ('Single', 'Multiple'))
