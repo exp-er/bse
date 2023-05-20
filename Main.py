4b5f99bc3d38635895a2f746ead96bb8cc4f348c
exper <akshat.pande@outlook.com>
2023-05-20 20:20:42 +0100

    Remove Extra Files

--- Main.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-from streamlit_option_menu import option_menu
-import BSE
-
-st.title('BSE Simulator')
-
-st.sidebar.radio('Select the type of simulation', ('Single', 'Multiple'))
