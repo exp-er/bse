cf6d4e57d565ec52a02233d4f478cc4d740eaf06
exper <akshat.pande@outlook.com>
2023-05-20 20:17:04 +0100

    Revert "Removed Extra Files"
    This reverts commit e3ab7fa368821f0fef7b2eef744fd89ef49b3391.

---
+++ data/about.py
@@ -0,0 +1,7 @@
+import streamlit as st 
+
+
+def about():
+    st.title('Bristol Stock Exchange (BSE) Simulator')
+    st.header('Akshat Pande')
+    st.write('This is a simulator for the Bristol Stock Exchange (BSE).')
---
+++ data/contact.py
@@ -0,0 +1,13 @@
+import streamlit as st 
+import webbrowser
+
+
+
+def email():
+    webbrowser.open_new_tab('mailto:exp.er@outlook.com')
+
+def linkedin():
+    webbrowser.open_new_tab('https://www.linkedin.com/in/pandeakshat/')
+
+def portfolio():
+    webbrowser.open_new_tab('https://pandeakshat.github.io/')
